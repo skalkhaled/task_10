e8eca9af02a683443346dc4e4dba09999212eb88
Sana AlKhaled <sanaalkhaled@Sanas-MacBook-Pro.local>
2020-01-30 13:50:01 +0300

    Task10Done

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sanaalkhaled/Desktop/Development/task10/task_10/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- restaurants/models.py
+++ restaurants/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Restaurant(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=120)
     description = models.TextField()
     opening_time = models.TimeField()
@@ -8,4 +10,12 @@ class Restaurant(models.Model):
     logo = models.ImageField(upload_to='restaurant_logos', null=True, blank=True)
 
     def __str__(self):
-    	return self.name
+    	return "%s,%s" %(self.id,self.owner, self.name)
+	
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=3)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
